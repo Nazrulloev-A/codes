00b32bf5cf13e93c677bb1884e5bed037bb917a4
Amrullo <emrullah_29@outlook.com>
2019-04-15 17:49:27 -0400

    code

---
+++ pyramid/src/pyramid/printpyramid.java
@@ -0,0 +1,20 @@
+package pyramid;
+
+public class printpyramid {
+
+	public static void main(String[] args) {
+	
+	int rows =5;
+	
+	for(int i = 1;i<=10;++i) {
+		for(int j= 1;j<=i;++j) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+		
+		}
+
+	}
+
+
